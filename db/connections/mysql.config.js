31fab87333310c17d224cdd5fefc82ae62b44812
castanedadiaz <andresfcastaneda@yahoo.com>
2024-07-07 22:26:14 -0300

    conectando DB

--- db/connections/mysql.config.js
+++ db/connections/mysql.config.js
@@ -1,8 +1,22 @@
+// const config = {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'node'
+// }
+
+// Host: sql5.freesqldatabase.com
+// Database name: sql5718576
+// Database user: sql5718576
+// Database password: Dka9PnJ9Pe
+// Port number: 3306
+
 const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'node'
+    host: process.env.HOST_DB, 
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DB
 }
+
 export default config
