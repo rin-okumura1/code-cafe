9e303f4dd33066c340c77c72feb00404c47b113d
castanedadiaz <andresfcastaneda@yahoo.com>
2024-07-08 00:00:22 -0300

    new

--- db/connections/mysql.config.js
+++ db/connections/mysql.config.js
@@ -1,10 +1,10 @@
-const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'node'
-}
+// const config = {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'node'
+// }
 
 
 // Host: sql5.freesqldatabase.com
@@ -13,11 +13,11 @@ const config = {
 // Database password: Dka9PnJ9Pe
 // Port number: 3306
 
-// const config = {
-//     host: process.env.HOST_DB, 
-//     user: process.env.USER_DB,
-//     password: process.env.PASS_DB,
-//     database: process.env.DB
-// }
+const config = {
+    host: process.env.HOST_DB, 
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DB
+}
 
 export default config
